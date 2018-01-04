f7155b307d7e59486ca87655ffcae67273346438
xrayhunter <xrayhunter@hotmail.com>
2018-01-04 10:14:10 -0800

    Engine Update

--- GameProject/GameProject/Core/Application.cs
+++ GameProject/GameProject/Core/Application.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameProject.Core;
+
 
 namespace GameProject
 {
-    public class Application
+    public class Application : Engine
     {
+        public Application()
+        {
+            this.Initialized += Application_Initialized;
+            this.Update += Application_Update;
+            this.Render += Application_Render;
+            this.Destroy += Application_Destroy;
+        }
+
+        private void Application_Initialized()
+        {
+        }
+
+        private void Application_Render()
+        {
+        }
+
+        private void Application_Update(float deltaTime)
+        {
+        }
+        
+        private void Application_Destroy()
+        {
+        }
+
         static void Main(string[] args)
         {
+            new Application();
         }
     }
 }
---
+++ GameProject/GameProject/Core/Engine.cs
@@ -0,0 +1,69 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace GameProject.Core
+{
+    public abstract class Engine
+    {
+        #region Delegates/Events
+
+        public delegate void _Initialized_();
+        /// <summary>
+        /// Initialized is called when before the first update.
+        /// </summary>
+        public event _Initialized_ Initialized = null;
+
+        public delegate void _Update_(float deltaTime);
+        /// <summary>
+        /// Update is called every tick.
+        /// </summary>
+        public event _Update_ Update = null;
+
+        public delegate void _Render_();
+        /// <summary>
+        /// Render is called every frame.
+        /// </summary>
+        public event _Render_ Render = null;
+
+        public delegate void _Destroy_();
+        /// <summary>
+        /// Destroy is called when the engine is being destroyed...
+        /// </summary>
+        public event _Destroy_ Destroy = null;
+
+        #endregion
+
+        private RenderWindow window;
+
+        public Engine()
+        {
+            window = new RenderWindow(new SFML.Window.VideoMode(480, 560), "Game");
+        }
+
+        public void Construct()
+        {
+            Clock clock = new Clock();
+            Initialized?.Invoke();
+            while (window.IsOpen)
+            {
+                // Receive the current events...
+                window.DispatchEvents();
+
+                // Update the engine...
+                Update?.Invoke(clock.Restart().AsMilliseconds());
+
+                // Render the engine's screen.
+                Render?.Invoke();
+
+                // Update our screen or in OpenGL Swap frame buffers...
+                window.Display();
+            }
+            Destroy?.Invoke();
+        }
+
+        public RenderWindow GetRenderWindow()
+        {
+            return window;
+        }
+    }
+}
